f0a552eff83260f1f72c5860e86da3defc5b9283
rishidwd2129 <rishidwd2129@gmail.com>
2024-08-11 00:30:21 +0530

    added archive model

--- AugustaCRM/LeadManagement/models.py
+++ AugustaCRM/LeadManagement/models.py
@@ -22,3 +22,14 @@ class CallList(models.Model):
     timestamp = models.DateField(auto_now_add = True)
     status = models.CharField(max_length=20)
     attempt = models.IntegerField(default= 0)
+
+
+class ArchiveLeads(models.Model):
+    currenttimemillis = models.CharField(max_length= 50, primary_key=True)
+    name = models.CharField(max_length= 100, null = True)
+    email = models.CharField(max_length= 50, null = True)
+    phone = models.CharField(max_length = 50, null =True)
+    platform = models.CharField(max_length = 100, null = True)
+    timestamp = models.DateField(auto_now_add = True)
+    status = models.CharField(max_length=20)
+    attempt = models.IntegerField(default= 0)
