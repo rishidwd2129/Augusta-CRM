165010b9b04cd18b0c7bb7ecd48690f078cd2a3b
rishidwd2129 <rishidwd2129@gmail.com>
2024-08-10 19:18:45 +0530

    added CallList table model

--- AugustaCRM/LeadManagement/models.py
+++ AugustaCRM/LeadManagement/models.py
@@ -4,11 +4,21 @@ from django.db import models
 # Create your models here.
 class NewLeads(models.Model):
     currenttimemillis = models.CharField(max_length= 50, primary_key=True)
-    name = models.VarField(max_length= 100, null = True)
+    name = models.CharField(max_length= 100, null = True)
     email = models.CharField(max_length= 50, null = True)
     phone = models.CharField(max_length = 50, null =True)
     platform = models.CharField(max_length = 100, null = True)
     timestamp = models.DateField(auto_now_add = True)
     status = models.CharField(max_length=20)
-    
+    attempt = models.IntegerField(default= 0)
 
+
+class CallList(models.Model):
+    currenttimemillis = models.CharField(max_length= 50, primary_key=True)
+    name = models.CharField(max_length= 100, null = True)
+    email = models.CharField(max_length= 50, null = True)
+    phone = models.CharField(max_length = 50, null =True)
+    platform = models.CharField(max_length = 100, null = True)
+    timestamp = models.DateField(auto_now_add = True)
+    status = models.CharField(max_length=20)
+    attempt = models.IntegerField(default= 0)
